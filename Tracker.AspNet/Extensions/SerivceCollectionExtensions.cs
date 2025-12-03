9679c8429c3a0848e0fb41a240a331743fc332e7
User <ulyanovskiy.2001@mail.ru>
2025-12-03 10:17:14 +0300

    feat: resolve entities to tables names on services registration

--- Tracker.AspNet/Extensions/SerivceCollectionExtensions.cs
+++ Tracker.AspNet/Extensions/SerivceCollectionExtensions.cs
@@ -5,6 +5,7 @@ using Tracker.AspNet.Middlewares;
 using Tracker.AspNet.Models;
 using Tracker.AspNet.Services;
 using Tracker.AspNet.Services.Contracts;
+using Tracker.Core.Extensions;
 
 namespace Tracker.AspNet.Extensions;
 
@@ -21,7 +22,16 @@ public static class SerivceCollectionExtensions
     {
         ArgumentNullException.ThrowIfNull(options, nameof(options));
 
-        services.AddSingleton(options);
+
+        services.AddSingleton((provider) =>
+        {
+            if (options.Entities is { Length: > 0 })
+            {
+                var scopeFactory = provider.GetRequiredService<IServiceScopeFactory>();
+                AssignEntitiesToTables<TContext>(scopeFactory, options);
+            }
+            return options;
+        });
 
         services.AddSingleton<IETagGenerator, ETagGenerator>();
         services.AddSingleton<IETagService, ETagService<TContext>>();
@@ -52,7 +62,21 @@ public static class SerivceCollectionExtensions
     {
         ArgumentNullException.ThrowIfNull(options, nameof(options));
 
+        if (options.Entities is { Length: > 0 })
+        {
+            var scopeFactory = builder.ApplicationServices.GetRequiredService<IServiceScopeFactory>();
+            AssignEntitiesToTables<TContext>(scopeFactory, options);
+        }
+
         return builder.UseMiddleware<TrackerMiddleware>(options);
+    }
+
+    private static void AssignEntitiesToTables<TContext>(IServiceScopeFactory scopeFactory, GlobalOptions options) where TContext : DbContext
+    {
+        using var scope = scopeFactory.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<TContext>();
+        var tablesNames = dbContext.GetTablesNames(options.Entities);
+        options.Tables = new HashSet<string>([.. options.Tables, .. tablesNames]).ToArray();
     }
 
     public static IApplicationBuilder UseTracker<TContext>(this IApplicationBuilder builder, Action<GlobalOptions> configure)
