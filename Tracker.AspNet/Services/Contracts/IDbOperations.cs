3d75eefd3bee8b4a52444024df726cd994ec1294
User <ulyanovskiy.2001@mail.ru>
2025-12-04 15:38:33 +0300

    feat: summarize db operations interface into ISourceOperations interface

--- Tracker.AspNet/Services/Contracts/IDbOperations.cs
+++ Tracker.AspNet/Services/Contracts/IDbOperations.cs
@@ -1,5 +1,13 @@
 ﻿namespace Tracker.AspNet.Services.Contracts;
 
+public interface ISourceOperations
+{
+    string SourceId { get; }
+    Task<DateTimeOffset?> GetLastTimestamp(string key, CancellationToken token);
+    Task<IEnumerable<DateTimeOffset>> GetLastTimestamp(string[] keys, CancellationToken token);
+    Task<DateTimeOffset?> GetLastTimestamp(CancellationToken token);
+}
+
 public interface IDbOperations
 {
     //string ConnectionId { get; init; }
