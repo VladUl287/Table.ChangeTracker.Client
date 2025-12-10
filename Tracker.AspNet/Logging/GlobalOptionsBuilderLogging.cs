4f7e140428dd20f61fc215c37ce5e9bc9de8c97d
User <ulyanovskiy.2001@mail.ru>
2025-12-10 15:46:29 +0300

    refactor: global options builder

---
+++ Tracker.AspNet/Logging/GlobalOptionsBuilderLogging.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Tracker.AspNet.Logging;
+
+public static partial class GlobalOptionsBuilderLogging
+{
+    [LoggerMessage(Level = LogLevel.Warning, Message = "Table name {TableName} duplicated in global options")]
+    public static partial void LogOptionsTableNameDuplicated(this ILogger logger, string tableName);
+}
--- Tracker.AspNet/Services/GlobalOptionsBuilder.cs
+++ Tracker.AspNet/Services/GlobalOptionsBuilder.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System.Collections.Immutable;
+using Tracker.AspNet.Logging;
 using Tracker.AspNet.Models;
 using Tracker.AspNet.Services.Contracts;
 using Tracker.Core.Extensions;
@@ -29,27 +31,38 @@ public sealed class GlobalOptionsBuilder(IServiceScopeFactory scopeFactory) : IO
     public ImmutableGlobalOptions Build<TContext>(GlobalOptions options) where TContext : DbContext
     {
         using var scope = scopeFactory.CreateScope();
+
         var dbContext = scope.ServiceProvider.GetRequiredService<TContext>();
         var sourceIdGenerator = scope.ServiceProvider.GetRequiredService<ISourceIdGenerator>();
-
-        var tablesNames = dbContext.GetTablesNames(options.Entities);
-        var tables = new HashSet<string>([.. options.Tables, .. tablesNames]).ToImmutableArray();
-
-        var source = options.Source;
-        if (options is { Source: null, SourceOperations: null, SourceOperationsFactory: null })
-            source = sourceIdGenerator.GenerateId<TContext>();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<GlobalOptionsBuilder>>();
 
         var cacheControl = options.CacheControl ?? options.CacheControlBuilder?.Build();
+        var tables = GetAndCombineTablesNames(options, dbContext, logger);
 
         return new ImmutableGlobalOptions
         {
             Tables = tables,
-            Source = source,
+            Source = options.Source,
             Filter = options.Filter,
             Suffix = options.Suffix,
             CacheControl = cacheControl,
             SourceOperations = options.SourceOperations,
             SourceOperationsFactory = options.SourceOperationsFactory,
         };
+    }
+
+    private static ImmutableArray<string> GetAndCombineTablesNames<TContext>(
+        GlobalOptions options, TContext dbContext, ILogger<GlobalOptionsBuilder> logger) where TContext : DbContext
+    {
+        var tablesNames = new HashSet<string>();
+        foreach (var tableName in options.Tables ?? [])
+            if (!tablesNames.Add(tableName))
+                logger.LogOptionsTableNameDuplicated(tableName);
+
+        foreach (var tableName in dbContext.GetTablesNames(options.Entities ?? []))
+            if (!tablesNames.Add(tableName))
+                logger.LogOptionsTableNameDuplicated(tableName);
+
+        return [.. tablesNames];
     }
 }
