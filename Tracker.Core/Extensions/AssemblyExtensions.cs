1c46bf58712b467842fc5eced68f05a024a44265
User <ulyanovskiy.2001@mail.ru>
2025-12-11 11:00:27 +0300

    refactor: GetAssemblyWriteTime

--- Tracker.Core/Extensions/AssemblyExtensions.cs
+++ Tracker.Core/Extensions/AssemblyExtensions.cs
@@ -7,20 +7,14 @@ public static class AssemblyExtensions
     public static DateTimeOffset GetAssemblyWriteTime(this Assembly assembly)
     {
         ArgumentNullException.ThrowIfNull(assembly, nameof(assembly));
+        ArgumentException.ThrowIfNullOrEmpty(assembly.Location, nameof(assembly.Location));
 
-        if (string.IsNullOrEmpty(assembly.Location))
-            throw new InvalidOperationException(
-                $"Cannot determine write time for assembly '{assembly.FullName}' " +
-                "because it has no physical location (may be dynamic or in-memory).");
-
-        var assemblyPath = assembly.Location;
-
-        if (!File.Exists(assemblyPath))
+        if (!File.Exists(assembly.Location))
             throw new FileNotFoundException(
                 $"Cannot determine write time for assembly '{assembly.FullName}'. " +
-                $"Assembly file not found at '{assemblyPath}'");
+                $"Assembly file not found at '{assembly.Location}'");
 
-        var lastWriteTimeUtc = File.GetLastWriteTimeUtc(assemblyPath);
+        var lastWriteTimeUtc = File.GetLastWriteTimeUtc(assembly.Location);
         return new DateTimeOffset(lastWriteTimeUtc);
     }
 }
