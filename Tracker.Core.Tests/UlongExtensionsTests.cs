ae19bf7c0d8f45191154a5f83809303f162bf47f
User <ulyanovskiy.2001@mail.ru>
2025-12-11 20:31:49 +0300

    tests: implement ulong tests

--- Tracker.Core.Tests/UlongExtensionsTests.cs
+++ Tracker.Core.Tests/UlongExtensionsTests.cs
@@ -1,5 +1,48 @@
-﻿namespace Tracker.Core.Tests;
+﻿using Tracker.Core.Extensions;
+
+namespace Tracker.Core.Tests;
 
 public class UlongExtensionsTests
 {
+    [Fact]
+    public void Count_Default()
+    {
+        // Arrange
+        ulong a = 123;
+        int req_count = 3;
+
+        // Act
+        var count = a.CountDigits();
+
+        // Assert
+        Assert.Equal(count, req_count);
+    }
+
+    [Fact]
+    public void Count_Zero()
+    {
+        // Arrange
+        ulong a = 0;
+        int req_count = 1;
+
+        // Act
+        var count = a.CountDigits();
+
+        // Assert
+        Assert.Equal(count, req_count);
+    }
+
+    [Fact]
+    public void Count_Max()
+    {
+        // Arrange
+        ulong a = ulong.MaxValue;
+        int req_count = 20;
+
+        // Act
+        var count = a.CountDigits();
+
+        // Assert
+        Assert.Equal(count, req_count);
+    }
 }
