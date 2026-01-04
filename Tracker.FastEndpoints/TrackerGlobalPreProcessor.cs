baac6df3ac1bf621d8602e5492b75246f53561fa
User <ulyanovskiy.2001@mail.ru>
2026-01-04 15:43:42 +0300

    feat: initilize fast endpoints extension of library

---
+++ Tracker.FastEndpoints/TrackerGlobalPreProcessor.cs
@@ -0,0 +1,24 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Http;
+using Tracker.AspNet.Models;
+using Tracker.AspNet.Services.Contracts;
+
+namespace Tracker.FastEndpoints;
+
+public sealed class TrackerGlobalPreProcessor : IGlobalPreProcessor
+{
+    public async Task PreProcessAsync(IPreProcessorContext ctx, CancellationToken ct)
+    {
+        var httpCtx = ctx.HttpContext;
+
+        var service = httpCtx.Resolve<IRequestHandler>();
+        var filter = httpCtx.Resolve<IRequestFilter>();
+        var opts = httpCtx.Resolve<ImmutableGlobalOptions>();
+
+        if (filter.ValidRequest(httpCtx, opts) && await service.HandleRequest(httpCtx, opts, ct))
+        {
+            await httpCtx.Response.SendResultAsync(Results.StatusCode(304));
+            return;
+        }
+    }
+}
